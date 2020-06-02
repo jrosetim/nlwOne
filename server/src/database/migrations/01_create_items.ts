eeb985db492528a4ad2ea58e4ee793afdc5276a9
Julio Cesar Rosetim <jrosetim@gmail.com>
2020-06-02 20:41:44 -0300

    nvl week

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    //create table
+    return knex.schema.createTable('items', table =>{
+                table.increments('id').primary();
+                table.string('image').notNullable();
+                table.string('title').notNullable();
+            })
+
+}
+
+export async function down(kenx : Knex) {
+    //drop table
+    return kenx.schema.dropTable('items');
+}
---
+++ server/src/database/migrations/02_create_points_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    //create table
+    return knex.schema.createTable('point_items', table =>{
+                table.increments('id').primary();
+                table.integer('point_id').notNullable().references('id').inTable('points');
+                table.integer('item_id').notNullable().references('id').inTable('items');
+            })
+
+}
+
+export async function down(kenx : Knex) {
+    //drop table
+    return kenx.schema.dropTable('point_items');
+}
