51cbef2a10bd2a60b7192dda01be32ab4a7ecf3c
Julio Cesar Rosetim <jrosetim@gmail.com>
2020-06-03 15:30:32 -0300

    ecoleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) =>{
+routes.get('/items', itemsController.index);
 
-    response.json({ message: 'Hello World'});
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
