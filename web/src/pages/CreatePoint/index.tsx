df01c9f6d5e238414c36c6c78868a1b80e146663
Julio Cesar Rosetim <jrosetim@gmail.com>
2020-06-04 08:50:42 -0300

    ecoleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 import {Map, TileLayer, Marker} from 'react-leaflet';
 import api from '../../services/api'
@@ -29,11 +29,21 @@ const CreatePoint = () => {
   const [ufs, setUf] = useState<string[]>([]);
   const [cities, setCities] = useState<string[]>([]);
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
+
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0 ,0]);
+
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0 ,0]);
+
+  const history = useHistory();
 
   useEffect( () => {
     api.get('items').then(response =>{
@@ -89,6 +99,50 @@ const CreatePoint = () => {
       event.latlng.lat,
       event.latlng.lng
     ])
+  };
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+    const {name, value} = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  function handleSelectItem(id: number){
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0){
+      const filteredItems = selectedItems.filter(item => item !== id)
+      setSelectedItems(filteredItems)
+    }else{
+      setSelectedItems( [ ...selectedItems, id ])
+    }
+  };
+
+  async function handleSubmit(event: FormEvent){
+    event.preventDefault();
+
+    const {name, email, whatsapp} = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data ={
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items      
+    };
+
+    await api.post('points', data);
+
+    alert('Ponto de coleta criado');
+
+    history.push('/');
   }
 
   return(
@@ -102,7 +156,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
         <fieldset>
           <legend>
@@ -114,6 +168,7 @@ const CreatePoint = () => {
               type="text"
               name="name"
               id="name"
+              onChange={handleInputChange}
             />
           </div>
           <div className="field-group">
@@ -123,6 +178,7 @@ const CreatePoint = () => {
                 type="email"
                 name="email"
                 id="email"
+                onChange={handleInputChange}
               />
             </div>
             <div className="field">
@@ -131,6 +187,7 @@ const CreatePoint = () => {
                 type="text"
                 name="whatsapp"
                 id="whatsapp"
+                onChange={handleInputChange}
               />
             </div>                      
           </div>
@@ -181,7 +238,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map(item => (
-              <li key={item.id}>
+              <li 
+                key={item.id} 
+                onClick={() => handleSelectItem(item.id)}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                >
                 <img src={item.item_url} alt={item.title}/>
                 <span>{item.title}</span>
               </li>
