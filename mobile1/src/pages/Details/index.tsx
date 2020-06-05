dcc77402648d36955d83c5b4ac6f9a251d7dff02
Julio Cesar Rosetim <julio.rosetim@elotech.com.br>
2020-06-05 13:37:59 -0300

    mobile

--- mobile1/src/pages/Details/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import { DefaultTheme } from '@react-navigation/native';
-
-const Details = () => {
-  return <View />
-}
-
-export default Details;
--- mobile1/src/pages/Points/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import { DefaultTheme } from '@react-navigation/native';
-
-const Points = () => {
-  return <View />
-}
-
-export default Points;
