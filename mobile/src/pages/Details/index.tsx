c27b28e72e12f5df254b7c08c02e8f2adc39b34f
Julio Cesar Rosetim <julio.rosetim@elotech.com.br>
2020-06-05 13:43:18 -0300

    mobile

---
+++ mobile/src/pages/Details/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+import { DefaultTheme } from '@react-navigation/native';
+
+const Details = () => {
+  return <View />
+}
+
+export default Details;
