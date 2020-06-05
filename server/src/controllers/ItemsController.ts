5458990e93eb4c7507c6899fb061e9b337930d9e
Julio Cesar Rosetim <julio.rosetim@elotech.com.br>
2020-06-05 16:21:55 -0300

    Pagina Points

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController{
             return {
               id: item.id,
               title: item.title,
-              item_url: `http://localhost:3333/uploads/${item.image}`
+              item_url: `http://192.168.100.57:3333/uploads/${item.image}`
             }
         })
     
